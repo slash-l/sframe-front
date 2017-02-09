98c9f9ca7ac00311d55342dc8ec2ca6d72a72464
liu <liu@mumu.local>
2017-02-10 00:13:55 +0800

    update html

--- javaScript/helloWorld.js
+++ javaScript/helloWorld.js
@@ -7,3 +7,32 @@ var c = a + b;
 console.log(c);
 
 //alert("ok");
+
+function A(){
+	this.sayHehe = function(){
+		console.log("hello sayHehe");
+	};
+}
+
+//实例方法
+A.prototype.sayHello = function(){
+	console.log("hello sayHello");
+}
+//静态方法
+A.sayHi = function(){
+	console.log("hello sayHi");
+}
+
+var a = new A();
+a.sayHello();
+A.sayHi();
+
+//继承类
+function B(){
+
+}
+B.prototype = new A();
+
+var b = new B();
+b.sayHello();
+b.sayHehe();
